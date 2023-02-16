3b287a56339aa0bf483f5ecaf9fa7f2b385211db
IFIX404 <williamandres321@gmail.com>
2023-02-16 15:51:09 -0500

    improving functionality.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const operaciones = document.querySelector(".operaciones");
 const resultados = document.querySelector(".resultados");
 const teclas = document.querySelectorAll(".teclas");
+let esperandoNumeroParaRaiz = false;
 
 teclas.forEach((tecla) => {
   tecla.addEventListener("click", (event) => {
@@ -9,19 +10,37 @@ teclas.forEach((tecla) => {
       case "AC":
         operaciones.value = "";
         resultados.value = "";
+        esperandoNumeroParaRaiz = false;
+        break;
+      case "√":
+        esperandoNumeroParaRaiz = true;
+        break;
+      case "𝜋":
+        operaciones.value += "math.pi";
+        esperandoNumeroParaRaiz = false;
         break;
       case "=":
         try {
-          resultados.value = eval(operaciones.value);
+          const expresion = operaciones.value;
+          console.log(expresion);
+          // Usamos math.evaluate en lugar de eval para evaluar la expresión matemática
+          resultados.value = math.evaluate(expresion);
         } catch (e) {
           resultados.value = "Error";
         }
+        esperandoNumeroParaRaiz = false;
         break;
       case "⌫":
         operaciones.value = operaciones.value.slice(0, -1);
+        esperandoNumeroParaRaiz = false;
         break;
       default:
-        operaciones.value += teclaPresionada;
+        if (esperandoNumeroParaRaiz) {
+          operaciones.value += `sqrt(${teclaPresionada})`;
+          esperandoNumeroParaRaiz = false;
+        } else {
+          operaciones.value += teclaPresionada;
+        }
     }
   });
 });
