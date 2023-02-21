daec99a30e68d8fa6bfe40ca7613d785dde2befb
IFIX404 <williamandres321@gmail.com>
2023-02-20 20:08:18 -0500

    js structure improvements

--- index.js
+++ index.js
@@ -3,42 +3,46 @@ const resultados = document.querySelector(".resultados");
 const teclas = document.querySelectorAll(".teclas");
 let operador = '';
 
+const actions = {
+  AC: () => {
+    operaciones.value = "";
+    resultados.value = "";
+  },
+  "√": () => {
+    operador = '√';
+    resultados.value = '';
+    operaciones.value += "√";
+  },
+  "𝜋": () => {
+    operaciones.value += "π";
+  },
+  "=": () => {
+    try {
+      const expresion = operaciones.value.replace(/π/, "(3.1415927)");
+      console.log(expresion);
+      resultados.value = math.evaluate(expresion);
+    } catch (e) {
+      resultados.value = "Error";
+    }
+  },
+  "⌫": () => {
+    operaciones.value = operaciones.value.slice(0, -1);
+  },
+  default: (teclaPresionada) => {
+    if (operador === '√') {
+      operaciones.value += teclaPresionada;
+      resultados.value = math.evaluate(`sqrt(${teclaPresionada})`);
+      operador = '';
+    } else {
+      operaciones.value += teclaPresionada;
+    }
+  }
+}
+
 teclas.forEach((tecla) => {
   tecla.addEventListener("click", (event) => {
     const teclaPresionada = event.target.innerText;
-    switch (teclaPresionada) {
-      case "AC":
-        operaciones.value = "";
-        resultados.value = "";
-        break;
-      case "√":
-        operador = '√';
-        resultados.value = '';
-        operaciones.value += "√";
-        break;
-      case "𝜋":
-        operaciones.value += "π";
-        break;
-      case "=":
-        try {
-          const expresion = operaciones.value.replace(/π/, "(3.1415927)");
-          console.log(expresion);
-          resultados.value = math.evaluate(expresion);
-        } catch (e) {
-          resultados.value = "Error";
-        }
-        break;
-      case "⌫":
-        operaciones.value = operaciones.value.slice(0, -1);
-        break;
-      default:
-        if (operador === '√') {
-          operaciones.value += teclaPresionada;
-          resultados.value = math.evaluate(`sqrt(${teclaPresionada})`);
-          operador = '';
-        } else {
-          operaciones.value += teclaPresionada;
-        }
-    }
+    const action = actions[teclaPresionada] || actions.default;
+    action(teclaPresionada);
   });
 });
